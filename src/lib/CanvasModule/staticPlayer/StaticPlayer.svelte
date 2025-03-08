c395c58343846ea515a5b86bb34490787fd452f3
bilza2023 <bilza2023@gmail.com>
2025-03-09 01:08:50 +0500

    a good thing about my programming is that now the things END they are not unlimited

---
+++ src/lib/CanvasModule/staticPlayer/StaticPlayer.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Player} from "$lib/taleem-canvas"
+    import { onMount } from "svelte";
+/////////////////////////////////////////////////////////////////
+    export let items = []; 
+    export let background = {}; // This is slideExtra for background item 
+    
+    let canvasElement;
+    let player; //to make it truly static even remove this so that this component is draw once.
+//////////////////////////////////////////////////////////////////
+onMount(async () => { if (canvasElement) {
+        const ctx = canvasElement.getContext("2d");
+        player = new Player(canvasElement, ctx);//Player
+        // player.background = background; // this is slideExtra
+        player.items = items;
+        player.draw();
+}});
+</script>
+  <canvas bind:this={canvasElement} ></canvas>
--- src/routes/static_player/+page.svelte
+++ src/routes/static_player/+page.svelte
@@ -1,6 +1,21 @@
 <script>
-    import StaticPlayer from "../../lib/CanvasPlayersModule/staticPlayer/StaticPlayer.svelte";
-
+    import StaticPlayer from "../../lib/CanvasModule/staticPlayer/StaticPlayer.svelte";
+    let items = [
+      {
+      uuid: "abc",
+      type: 'rectangle',
+      x: 100,
+      y: 100,
+      width: 100,
+      height: 100,
+      filled: true,
+      lineWidth: 1,
+      dash: 0,
+      gap: 0,
+      color: "red",
+      globalAlpha: 1
+    }
+    ]; 
 </script>
 
-  <StaticPlayer />
+  <StaticPlayer {items} />
