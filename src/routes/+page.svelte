ac4b8170bb6f9700a038b6fa04b6775fb7cf2c45
bilza2023 <bilza2023@gmail.com>
2025-04-03 09:46:23 +0500

    best landing page ever made

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,59 @@
-    
 <script>
+    const cards = [
+        {
+        title: 'Presentations',
+        image: 'presentation.jpeg',
+        comments: 'Presentation templates',
+        url : ""
+      },
 
-</script>
+      {
+        title: 'The Editor',
+        image: 'magic.jpeg',
+        comments: 'Edit presentations',
+        url : ""
+      },
+      {
+        title: 'The Player',
+        image: 'player.jpeg',
+        comments: 'Play presentations',
+        url : ""
+      },
+      {
+        title: 'Experimentation',
+        image: 'exp.jpeg',
+        comments: 'Testing new features',
+        url : ""
+      },
+      {
+        title: 'Canvas Slides',
+        image: 'canvas.jpeg',
+        comments: 'Examples of canvas slides',
+        url : "canvas_examples"
+      }
+    ];
+  </script>
+  
+  <div class="bg-gray-800 text-white p-0 m-0 ">
+   
+    <h1 class="text-4xl font-bold text-center py-4 shadow-md bg-gradient-to-r from-purple-800 to-indigo-600 rounded-lg m-0 pt-4">
+      Taleem App
+    </h1>
+  
+    <div class="flex flex-wrap justify-center gap-4 p-4">
+      {#each cards as card}
+      
+        <a href={card.url}>
+        <div class="bg-gray-800 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 p-4 w-64 shadow-red-600 hover:shadow-green-500 cursor-pointer">
+        
+            <img src={"./app/" + card.image} alt={card.title} class="rounded-md mb-2 mx-auto" />
+        
+            <h2 class="text-lg font-semibold text-center mb-1">{card.title}</h2>
+        
+            <p class="text-sm text-gray-400 text-center">{card.comments}</p>
+        </div>
+        </a>
 
-<br/>
-    <br/>
-    <br/>
-<h1 style="color:white;">Html Taleem Canvas Test Pages</h1>
-<br/>
-    <br/>
-    <br/>
-<div class="text-white">
-    <h1><b><a class="text-white" href="/static_slides">static_slides</a></b></h1>
-    <h1><b><a class="text-white" href="/presentation_editor">presentation_editor</a></b></h1>
-    <h1><b><a class="text-white" href="/presentation_player">presentation_player</a></b></h1>
-    <br>
-    <h1><b><a class="text-white" href="/eqs_editor">eqs_editor</a></b></h1>
-    <h1><b><a class="text-white" href="/canvas_examples">canvasExamples</a></b></h1>
-    <br/>
-    <br/>
-    <br/>
-    
-    <h1><b><a class="text-white" href="/canvas_editor">CanvasEditor.svelte</a></b></h1>
-    <h1><b><a class="text-white" href="/canvas_player">canvas_player</a></b></h1>
-    <h1><b><a class="text-white" href="/raw_core">raw_core</a></b></h1>
-    <h1><b><a class="text-white" href="/raw_editor">raw_editor</a></b></h1>
-    
-
-</div>
-
-<br/>
-<br/>
-<br/>
-
-
-<style>
-
-    .text-white{
-        color: aliceblue;
-    }
-</style>
+      {/each}
+    </div>
+  </div>
